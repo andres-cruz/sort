e9fbc0423b713d96ae1d13a781fb5fbf1c2e07c0
Andres <andrescruz.tech@gmail.com>
2019-09-29 19:51:33 -0300

    Deleted build files

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,24 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, 88, 9, 22, 32]);
+const numbersCollection = new NumbersCollection([
+  45,
+  -12,
+  10,
+  3,
+  -5,
+  0,
+  88,
+  9,
+  22,
+  32
+]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
+const charactersCollection = new CharactersCollection(
+  'Xaybijkwiewhdksjubsjwojsnyqrafshokqsjkanchyk'
+);
 charactersCollection.sort();
 console.log(charactersCollection.data);
 
@@ -16,6 +29,10 @@ linkedList.add(500);
 linkedList.add(-50);
 linkedList.add(-3);
 linkedList.add(4);
+linkedList.add(23);
+linkedList.add(48);
+linkedList.add(89);
+linkedList.add(24);
 
 linkedList.sort();
 linkedList.print();
